c6e986e14f1174a054d658dee6805d41362be2d3
Hanh109 <96662137+Hanh109@users.noreply.github.com>
2022-02-05 12:13:15 +0700

    Add files via upload

---
+++ VARKALA/1.js
@@ -0,0 +1,10 @@
+﻿document.addEventListener('DOMContendLoaded',function(){
+    var trangthai ='duoi1000';
+    var anh1=document.querySelector('.anh1');
+    var anh2 =document.querySelector('.anh2');
+    console.log(anh1.offsetTop);
+    console.log(anh2.offsetTop);
+  
+    console.log('he');
+
+})
